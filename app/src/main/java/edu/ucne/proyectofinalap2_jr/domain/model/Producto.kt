09937feca1eaf49e94edbe4a1ff7526bbeafddcc
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-30 23:48:48 -0400

    Crea el Producto.kt

---
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/domain/model/Producto.kt
@@ -0,0 +1,11 @@
+package edu.ucne.proyectofinalap2_jr.domain.model
+
+data class Producto(
+    val productoId: String = "",
+    val nombre: String = "",
+    val descripcion: String = "",
+    val precio: Double = 0.0,
+    val imagen: String = "",
+    val categoriaId: String = "",
+    val disponible: Boolean = true
+)
