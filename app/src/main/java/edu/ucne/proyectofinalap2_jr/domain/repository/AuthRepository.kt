8d964e9f8a14f97025f1aae1f524430f33259d3e
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-30 23:47:26 -0400

    Modifica el AuthRepository.kt

--- app/src/main/java/edu/ucne/proyectofinalap2_jr/domain/repository/AuthRepository.kt
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/domain/repository/AuthRepository.kt
@@ -2,9 +2,13 @@ package edu.ucne.proyectofinalap2_jr.domain.repository
 
 import android.content.Context
 import com.google.firebase.auth.FirebaseUser
+import edu.ucne.proyectofinalap2_jr.domain.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     suspend fun signInWithGoogle(context: Context): Result<FirebaseUser>
     suspend fun signOut()
     fun getCurrentUser(): FirebaseUser?
+    suspend fun getUserData(uid: String): Usuario?
+    suspend fun saveUserData(usuario: Usuario)
 }
