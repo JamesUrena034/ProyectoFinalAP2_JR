6a96ee8a05626fc4eb7cb4bd7356c8a9cfa2dfb7
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-30 23:48:22 -0400

    Crea el Pedido.kt

---
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/domain/model/Pedido.kt
@@ -0,0 +1,18 @@
+package edu.ucne.proyectofinalap2_jr.domain.model
+
+data class Pedido(
+    val pedidoId: String = "",
+    val usuarioId: String = "",
+    val productos: List<ItemPedido> = emptyList(),
+    val total: Double = 0.0,
+    val estado: String = "pendiente",
+    val fecha: String = ""
+)
+
+data class ItemPedido(
+    val productoId: String = "",
+    val nombre: String = "",
+    val precio: Double = 0.0,
+    val cantidad: Int = 1,
+    val imagen: String = ""
+)
