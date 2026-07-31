52f6ec7a61f6af7cbd5cd1afc3a9c08d47f22ab2
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-30 23:47:56 -0400

    Crea el Categoria.kt

---
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/domain/model/Categoria.kt
@@ -0,0 +1,8 @@
+package edu.ucne.proyectofinalap2_jr.domain.model
+
+data class Categoria(
+    val categoriaId: String = "",
+    val nombre: String = "",
+    val descripcion: String = "",
+    val imagen: String = ""
+)
