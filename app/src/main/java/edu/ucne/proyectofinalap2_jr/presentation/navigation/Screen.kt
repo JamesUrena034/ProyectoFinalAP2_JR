a43d6ff70ec68f0f26253e9eab6e99eba128c754
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-08-01 16:44:10 -0400

    Crea la Screen para la Navegacion

---
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/presentation/navigation/Screen.kt
@@ -0,0 +1,30 @@
+package edu.ucne.proyectofinalap2_jr.presentation.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Screen : NavKey {
+
+    @Serializable data object Login : Screen()
+
+    @Serializable data object Home : Screen()
+
+    @Serializable data object Categorias : Screen()
+
+    @Serializable data class ProductoDetail(val productoId: String) : Screen()
+
+    @Serializable data class ProductoCreate(val productoId: String = "") : Screen()
+
+    @Serializable data object Carrito : Screen()
+
+    @Serializable data object MisPedidos : Screen()
+
+    @Serializable data class DetallePedido(val pedidoId: String) : Screen()
+
+    @Serializable data object Perfil : Screen()
+
+    @Serializable data object AdminPedidos : Screen()
+
+    @Serializable data object CreateCategoria : Screen()
+}
