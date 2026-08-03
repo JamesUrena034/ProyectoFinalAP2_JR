1646c9848932dc4e64576d08c94d67fd08ca5d44
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-08-03 19:54:10 -0400

    Elimina Archivos inecesarios

--- app/src/main/java/edu/ucne/proyectofinalap2_jr/presentation/login/AuthUiEvent.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.proyectofinalap2_jr.presentation.login
-
-import android.content.Context
-
-sealed class AuthUiEvent {
-    data class SignInWithGoogle(val context: Context) : AuthUiEvent()
-    object SignOut : AuthUiEvent()
-}
