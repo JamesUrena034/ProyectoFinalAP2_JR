500905aecb64c1e80a1a0f4babb98fd879d7fe44
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-08-03 19:10:42 -0400

    Agrega Pantallas al MainActivity.kt

--- app/src/main/java/edu/ucne/proyectofinalap2_jr/MainActivity.kt
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/MainActivity.kt
@@ -3,15 +3,20 @@ package edu.ucne.proyectofinalap2_jr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.proyectofinalap2_jr.presentation.login.AuthScreen
+import edu.ucne.proyectofinalap2_jr.presentation.navigation.MainNavigationDisplay
+import edu.ucne.proyectofinalap2_jr.ui.theme.ProyectoFinalAP2_JRTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            AuthScreen()
+            ProyectoFinalAP2_JRTheme {
+                MainNavigationDisplay()
+            }
         }
     }
 }
