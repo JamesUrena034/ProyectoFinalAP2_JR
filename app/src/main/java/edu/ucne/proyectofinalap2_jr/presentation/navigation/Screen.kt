25579f58acd66b05131065d5c581a6f662970da1
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-08-04 21:33:46 -0400

    Modifica el Screen.kt para anadir EditCategoria

--- app/src/main/java/edu/ucne/proyectofinalap2_jr/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/proyectofinalap2_jr/presentation/navigation/Screen.kt
@@ -6,42 +6,18 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed class Screen {
 
-    @Serializable
-    data object Login : Screen()
-
-    @Serializable
-    data object Home : Screen()
-
-    @Serializable
-    data object Categorias : Screen()
-
-    @Serializable
-    data class ProductoDetail(val productoId: String) : Screen()
-
-    @Serializable
-    data class ProductoCreate(val productoId: String = "") : Screen()
-
-    @Serializable
-    data object AdminProductos : Screen()
-
-    @Serializable
-    data object Carrito : Screen()
-
-    @Serializable
-    data object MisPedidos : Screen()
-
-    @Serializable
-    data class DetallePedido(val pedidoId: String) : Screen()
-
-    @Serializable
-    data object Perfil : Screen()
-
-    @Serializable
-    data object AdminPedidos : Screen()
-
-    @Serializable
-    data object CreateCategoria : Screen()
-
-    @Serializable
-    data class EditCategoria(val categoriaId: String) : Screen()
+    @Serializable data object Login : Screen()
+    @Serializable data object Home : Screen()
+    @Serializable data object Categorias : Screen()
+    @Serializable data class ProductoDetail(val productoId: String) : Screen()
+    @Serializable data class ProductoCreate(val productoId: String = "") : Screen()
+    @Serializable data object AdminProductos : Screen()
+    @Serializable data object Carrito : Screen()
+    @Serializable data object MisPedidos : Screen()
+    @Serializable data class DetallePedido(val pedidoId: String) : Screen()
+    @Serializable data object Perfil : Screen()
+    @Serializable data object AdminPedidos : Screen()
+    @Serializable data object CreateCategoria : Screen()
+    @Serializable data class EditCategoria(val categoriaId: String) : Screen()
+    @Serializable data class ProductosPorCategoria(val categoriaId: String, val categoriaNombre: String) : Screen()
 }
